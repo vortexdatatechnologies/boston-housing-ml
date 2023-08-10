6d070ecc0f2c6ab01685718db35e894fc1c5b3a7
vortexdatatechnologies <139167026+vortexdatatechnologies@users.noreply.github.com>
2023-08-10 20:03:57 +0200

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,49 +3,66 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+import joblib
 
 # Load the Boston Housing dataset
-def preprocess_data():
-    boston_dataset = fetch_openml(name="boston", version=1, as_frame=True)
-    boston = boston_dataset.frame
+def load_data():
+    return fetch_openml(name="boston", version=1, as_frame=True).frame
 
-    # Split the data into training set and test set
-    train_set, test_set = train_test_split(boston, test_size=0.2, random_state=42)
+# Splitting the data
+def split_data(data, test_size=0.2, random_state=42):
+    return train_test_split(data, test_size=test_size, random_state=random_state)
 
-    # Data cleaning: fill NA values with the column mean
-    train_set.fillna(train_set.mean(), inplace=True)
-    test_set.fillna(train_set.mean(), inplace=True)  # use mean from training set
+# Handling missing values
+def handle_missing_values(train_set, test_set, numerical_cols):
+    for df in [train_set, test_set]:
+        for col in numerical_cols:
+            mean_val = train_set[col].mean()
+            df[col] = df[col].fillna(mean_val)
+    return train_set, test_set
 
-    # Check for duplicate rows in the training set
+# Removing duplicates
+def remove_duplicates(train_set):
     duplicates = train_set.duplicated()
-    print(f"Number of duplicate rows = {duplicates.sum()}")
-
-    # Remove duplicates if any
     if duplicates.sum() > 0:
-        train_set = train_set[~duplicates]
+        train_set = train_set.loc[~duplicates]
+    return train_set
 
-    # Feature Engineering: Add some new features (for example, square of LSTAT and log of CRIM)
-    train_set['LSTAT_squared'] = train_set['LSTAT'] ** 2
-    train_set['CRIM_log'] = np.log(train_set['CRIM'])
+# Feature Engineering
+def feature_engineering(train_set, test_set):
+    for df in [train_set, test_set]:
+        df['LSTAT_squared'] = df['LSTAT'] ** 2
+        df['CRIM_log'] = np.log(df['CRIM'])
+    return train_set, test_set
 
-    # Apply same transformations to the test set
-    test_set['LSTAT_squared'] = test_set['LSTAT'] ** 2
-    test_set['CRIM_log'] = np.log(test_set['CRIM'])
+# Separating the features and labels
+def separate_features_labels(train_set, test_set):
+    X_train = train_set.drop('MEDV', axis=1)
+    y_train = train_set['MEDV']
+    X_test = test_set.drop('MEDV', axis=1)
+    y_test = test_set['MEDV']
+    return X_train, X_test, y_train, y_test
 
-    # Separate the features from the labels
-    train_set_features = train_set.drop('MEDV', axis=1)
-    train_set_labels = train_set['MEDV']
-
-    test_set_features = test_set.drop('MEDV', axis=1)
-    test_set_labels = test_set['MEDV']
-
-    # Feature Scaling: Standardize the features
+# Scaling the features
+def scale_features(X_train, X_test):
     scaler = StandardScaler()
-    train_set_scaled = pd.DataFrame(scaler.fit_transform(train_set_features), columns=train_set_features.columns)
-    test_set_scaled = pd.DataFrame(scaler.transform(test_set_features), columns=test_set_features.columns)
-    return train_set_scaled, test_set_scaled, train_set_labels, test_set_labels
+    X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns)
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), columns=X_test.columns)
+    # Save the scaler object for future use
+    joblib.dump(scaler, 'scaler.pkl')
+    return X_train_scaled, X_test_scaled
 
-# Now your data is ready for machine learning modeling!
+def preprocess_data():
+    boston = load_data()
+    train_set, test_set = split_data(boston)
+    numerical_cols = train_set.select_dtypes(include=['float64', 'int64']).columns
+    train_set, test_set = handle_missing_values(train_set, test_set, numerical_cols)
+    train_set = remove_duplicates(train_set)
+    train_set, test_set = feature_engineering(train_set, test_set)
+    X_train, X_test, y_train, y_test = separate_features_labels(train_set, test_set)
+    X_train_scaled, X_test_scaled = scale_features(X_train, X_test)
+    return X_train_scaled, X_test_scaled, y_train, y_test
+
 if __name__ == "__main__":
-    train_set_scaled, test_set_scaled, train_set_labels, test_set_labels = preprocess_data()
-    print(train_set_scaled.head())
+    X_train_scaled, X_test_scaled, y_train, y_test = preprocess_data()
+    print(X_train_scaled.head())
